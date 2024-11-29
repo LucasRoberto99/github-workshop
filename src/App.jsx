7fff29c62b4ced4ec3ee32970e5d49a7fe9a863c
LucasRoberto99 <lucasr.prof@gmail.com>
2024-11-29 11:13:15 +0100

    added Header comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+      <h1>Homy</h1>
     </div>
   );
 }
