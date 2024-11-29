00b5d47da7d60b875253669704b9eb61402b7ee4
Bastien Lairis <bastien.lairis@gmail.com>
2024-11-29 12:01:58 +0100

    Update Main.jsx on bastien-front branch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
 export default function Main() {
   return (
     <main style={{ background: "salmon", padding: "2rem" }}>
-      <h2>Composant Main créé par bastien sur la branche bastien-branch</h2>
+      <h2>
+        Composant Main créé par bastien sur la branche bastien-branch / et
+        rajout de texte depuis la nouvelle branche bastien-front
+      </h2>
     </main>
   );
 }
