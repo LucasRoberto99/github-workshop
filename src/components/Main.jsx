c0e07d524012c7086b726f7b92088268217a15e3
Bastien Lairis <bastien.lairis@gmail.com>
2024-11-29 11:51:57 +0100

    Add Main.jsx component on bastien-branch

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main style={{ background: "salmon", padding: "2rem" }}>
+      <h2>Composant Main créé par bastien sur la branche bastien-branch</h2>
+    </main>
+  );
+}
